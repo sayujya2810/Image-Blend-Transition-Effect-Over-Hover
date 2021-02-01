2eb8a3d429b3c159eda0b7ec8c5e608330c3f4ad
Sayujya Malkan <sayujyamalkan@gmail.com>
2021-02-01 14:02:06 +0530

    Add files via upload

---
+++ appBlendImage.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent:document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: 'MonoChromePic0.jpg',
+    image2: 'MonoChromePic1.jpg',
+    displacementImage: 'dis.png'
+});
